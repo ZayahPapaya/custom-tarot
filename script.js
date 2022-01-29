47b98d27a6fcb190af3b4a703c1785a63e175137
Aiden <hyena.shenanigans@gmail.com>
2022-01-29 14:47:41 -0800

    javascript changes

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict'
+// This is industry standard to enforce tighter grammar for scripts
+// alert('Spooky');
+// let yeet = prompt('yeet');
+let message = prompt('Yote');
+document.write(message);
