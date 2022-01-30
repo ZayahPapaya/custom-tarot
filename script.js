9b3bae64ac1fae1eca9aac71abe000bf2552c60c
Aiden <hyena.shenanigans@gmail.com>
2022-01-29 16:42:52 -0800

    minor

--- script.js
+++ script.js
@@ -4,3 +4,18 @@
 // let yeet = prompt('yeet');
 let message = prompt('Yote');
 document.write(message);
+function myFunction() {
+    document.getElemmentByID("Dropdown").classList.toggle("show")
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.btnMaj')) {
+        let dropdowns = document.getElemmentsByClassName("btnMajcontent");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
